c9830ce3fa4d4eef60cc29af6ed089eadf9a4184
directormac <markasena@gmail.com>
2023-07-25 23:22:38 +0800

    Added fwdp assets

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import './styles/style.css'
-import javascriptLogo from './assets/javascript.svg'
-import { setupCounter } from './components/counter.js'
+import './styles/style.css';
+import javascriptLogo from './assets/javascript.svg';
+import fwdpLogo from '/fwdp-stacked-tr.png';
+import fwdpSvg from '/fwdp.svg';
+import { setupCounter } from './components/counter.js';
 
 // From public folder
-import viteLogo from '/vite.svg'
+import viteLogo from '/vite.svg';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -13,14 +15,25 @@ document.querySelector('#app').innerHTML = `
     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
-    <h1>Hello Vite!</h1>
+    <a href="https://github.com/FWDP/" target="_blank">
+      <img src="${fwdpLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Filipino Web Dev Peers!</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-      Click on the Vite logo to learn more
+      Click on the logos to learn more!
     </p>
+    <div class="footnote">
+      <p>
+        Made with 💓 , Visit us at
+      </p>
+      <a href="https://www.facebook.com/fwdpeers"> 
+        <img src="${fwdpSvg}" class="logo fwdpLogo" alt="FWDP logo" /> 
+      </a>
+    </div>
   </div>
-`
+`;
 
-setupCounter(document.querySelector('#counter'))
+setupCounter(document.querySelector('#counter'));
