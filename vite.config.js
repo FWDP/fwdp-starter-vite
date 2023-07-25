abbf76d2a891c857e08ee69355a10e99ff73a5e0
directormac <markasena@gmail.com>
2023-07-25 17:25:08 +0800

    init

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+
+
+export default defineConfig({
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  test: {
+    include: ['**/*.test.js'],
+  }
+})
