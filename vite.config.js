e0cab42dd467290e752d40ab2ceb547d917f5067
directormac <markasena@gmail.com>
2023-07-25 22:05:27 +0800

    Fix Dev vars

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 
+// const dev = process.argv.includes('dev');
+// const dev = process.env.NODE_ENV === ''
+// const BASE = dev ? '/' : '/vanillawithbatteries';
+
 export default defineConfig({
-  base: 'vanillawithbatteries/',
+  base: process.env.NODE_ENV === 'production' ? '/vanillawithbatteries/' : '/',
   build: {
     outDir: './dist',
     emptyOutDir: true,
